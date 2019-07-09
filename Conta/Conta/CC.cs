d4c2b76e394a4f16d4af2cd3622dbd4e8dd4d289
jonataferreira <52683250+jonataferreira@users.noreply.github.com>
2019-07-08 22:06:38 -0300

    Add files via upload

---
+++ Conta/Conta/CC.cs
@@ -0,0 +1,45 @@
+﻿namespace Conta
+{
+    class CC
+    {
+        private int _cod;
+        private int _id;
+        private string _titular;
+        private double _saldo;
+
+        public double Saldo { get => _saldo; set => _saldo = value; }
+        public string Titular { get => _titular; set => _titular = value; }
+        public int Cod { get => _cod; private set => _cod = value; }
+        public int Id { get => _id; private set => _id = value; }
+
+        public CC(string titular, double saldo, int cod, int id)
+        {
+
+            this.Titular = titular;
+            this.Saldo = saldo;
+            this.Cod += 1;
+            this.Id = id;
+
+        }
+
+
+        public CC(double saldo, int teste)
+        { 
+            if (teste == 2)
+            {
+                this.Saldo += saldo;
+            }
+            else
+            {
+                this.Saldo -= (5 + saldo);
+            }
+
+
+        }
+
+        public override string ToString()
+        {
+            return "Titular da conta: " + _titular + "\nSaldo em conta: R$" + _saldo.ToString("F2") + Cod;
+        }
+    }
+}
---
+++ Conta/Conta/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Conta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            
+            Console.WriteLine("Digite o número da operação: 1 - Inclusão; 2 - Depósito; 3 - Saque; ou qualquer outro valor para sair");
+            int teste = int.Parse(Console.ReadLine());
+
+
+            if (teste == 1)
+            {
+                Console.WriteLine("Digite a Identidade do correntista");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o nome completo do correntista");
+                string Titular = Console.ReadLine();
+                Console.WriteLine("O valor de depósito inicial");
+                double Saldo = Double.Parse(Console.ReadLine());
+                int cod = 0;
+
+                CC c;
+                c = new CC(Titular, Saldo, cod, id);
+                Console.WriteLine(c);
+            }
+
+            if (teste == 2)
+            {
+
+                Console.WriteLine("Digite o valor do depósito");
+                double Saldo = Double.Parse(Console.ReadLine());
+                CC c = new CC(Saldo, teste);
+                Console.WriteLine(c);
+            }
+
+            if (teste == 3)
+            {
+                Console.WriteLine("O valor do Saque (haverá uma cobrança de R$5,00)");
+                double Saldo = (Double.Parse(Console.ReadLine()));
+                CC c;
+                c = new CC(Saldo, teste);
+
+                Console.WriteLine(c);
+            }
+
+
+            else
+            {
+                Console.WriteLine("O BANCO XXXXX AGRADECE!");
+            }
+
+
+        }
+    }
+}
